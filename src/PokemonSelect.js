81d348cf1d3556fc65aa2112b7324d2e694a8009
Luis Irizarry <lirizarry6178@gmail.com>
2025-01-31 18:08:32 -0500

    finished part 4, fixing useAxios to allow dynamic urls and still allowing it to work as intended originally

--- src/PokemonSelect.js
+++ src/PokemonSelect.js
@@ -18,8 +18,8 @@ function PokemonSelect({ add, pokemon = pokemonList }) {
           </option>
         ))}
       </select>
-      <button onClick={() => add(pokemon[pokeIdx])}>Catch one!</button>
-      <button onClick={() => add(choice(pokemon))}>I'm feeling lucky</button>
+      <button onClick={() => add(`/${pokemon[pokeIdx]}`)}>Catch one!</button>
+      <button onClick={() => add(`/${choice(pokemon)}`)}>I'm feeling lucky</button>
     </div>
   );
 }
--- src/hooks/useAxios.jsx
+++ src/hooks/useAxios.jsx
@@ -2,17 +2,25 @@ import { useState } from "react";
 import { v4 as uuid } from "uuid";
 import axios from "axios";
 
-const useAxios = (baseUrl, restOfUrl = '') => {
+const useAxios = (baseUrl) => {
     const [data, setData] = useState([]);
 
-    const addNewObject = async () => {
-        try{
-            const res = await axios.get(`${baseUrl + restOfUrl}`);
-            setData(data => [...data, {...res.data, id: uuid()}])
-        } catch (e) {
-            console.error(`Error adding new object: ${e}`);
+    const addNewObject = async (restOfUrl = '') => {
+        // For some reason if I just pass the addNewObject in the onClick normally and not as an arrow function, React adds an object
+        // so even if I intend for 'restOfUrl' to be empty, it actually recieves an object
+        // So i added some error handling for that event
+
+        if (typeof restOfUrl === 'object' && restOfUrl !== null) {
+            restOfUrl = '';
         }
-    }
+        try {
+          const url = restOfUrl ? `${baseUrl}${restOfUrl}` : baseUrl;
+          const res = await axios.get(url);
+          setData(data => [...data, { ...res.data, id: uuid() }]);
+        } catch (error) {
+          console.error(`Error adding new object: ${error}`);
+        }
+      }
 
     return [data, addNewObject];
 }
